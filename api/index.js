682a6ee1ade43beeba54fcbef6073de224122947
yasmin20s <Yasmin.awod.22230436@commerce.helwan.edu.eg>
2025-11-11 21:14:54 +0200

    backend/register/addevents

--- api/index.js
+++ api/index.js
@@ -1,40 +1,29 @@
-const express = require("express");
-const app = express();
-
-const mongoose = require("mongoose");
-
-require("dotenv").config();
-
-const DB_URL = process.env.DB_URL;
-
-const PORT = 7000;
-
+const express=require('express');
+const app=express();
+const mongoose=require('mongoose');
+const user_router=require('../routes/users/auth');
+const event_router=require('../routes/events/eventsr');
+require('dotenv').config();
+const DB_URL=process.env.DB_URL;
+const PORT=7000;
 app.use(express.json());
-
-mongoose
-  .connect(DB_URL)
-  .then(() => {
+mongoose.connect(DB_URL).then(()=>{
     console.log("Connected to the database");
-  })
-  .catch((err) => {
+}).catch((err)=>{
     console.log(`Error connecting to the database due to ${err.message}`);
-  });
-
-app.get("/", (req, res) => {
-  res.status(200).json({
-    message: "Welcome On Home Back End",
-  });
 });
 
-app.use((req, res) => {
-  res.status(404).json({
-    message: "Route not found",
-    data: null,
-  });
+app.use("/api/v1/auth",user_router);
+app.use("/api/v1/events", event_router);
+
+app.use((req,res) => {
+    res.status(404).json({
+        message:"Route not found",
+        data:null,
+
+    });
 });
 
-app.listen(PORT, (err) => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(PORT,(err)=>{
+    console.log(`Server is running on port ${PORT}`);
 });
-
-module.exports = app;
