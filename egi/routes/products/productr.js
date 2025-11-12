396ffa447ba027eece8b5dc5c8096778c3d7bae0
Khalid Samy Abd-Elzaher <khalidsamy1231@gmail.com>
2025-11-12 06:49:46 +0200

    Fix bugs

--- egi/routes/products/productr.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const deleteProduct = require("../../controllers/products/deleteProduct");
-
-const router = express.Router();
-
-router.delete("/:id", deleteProduct);
-
-module.exports = router;
---
+++ model/product.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
+
+module.exports = mongoose.model("Product", productSchema);
