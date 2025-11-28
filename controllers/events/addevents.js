2c449f80948a59a8a9311e0673395d73e7d7fd3c
Khalid Samy Abdelzaher <138882916+khalidsamy@users.noreply.github.com>
2025-11-28 18:57:20 +0200

    Refactor event addition logic in addevents.js
    Removed event validation and creation logic from addevents.js.

--- controllers/events/addevents.js
+++ controllers/events/addevents.js
@@ -9,59 +9,6 @@ const addEvent = async (req, res) => {
             message: "Event added successfully",
             data: newEvent,
         });
-  try {
-    const {
-      title,
-      location,
-      category,
-      date,
-      openingHours,
-      entryFee,
-      description,
-      img
-    } = req.body;
-    if (
-      !title ||
-      !location ||
-      !category ||
-      !date ||
-      !openingHours ||
-      !openingHours.from ||
-      !openingHours.to ||
-      !entryFee ||
-      !description ||
-      !img
-    ) {
-      return res.status(400).json({
-        message: "All fields are required",
-        data: null,
-      });
-    }
-
-    const existingEvent = await Event.findOne({ description });
-
-    if (existingEvent) {
-      return res.status(400).json({
-        message: "This event already exists",
-        data: null,
-      });
-    }
-
-    const newEvent = new Event({
-      title,
-      location,
-      category,
-      date,
-      openingHours: {
-        from: openingHours.from,
-        to: openingHours.to
-      },
-      entryFee,
-      description,
-      img,
-    });
-
-    await newEvent.save();
 
     } catch (err) {
         console.error("Error adding event:", err);
